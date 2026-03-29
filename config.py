b5c14582115549095a7842b8d5e6a990a76ac55f
Elias Contrera <wooziecentury@gmail.com>
2026-03-29 00:52:45 -0300

    Buscador Centralizado v1.1
    - Configuración para levantarlo con render
    - Se usa PostgreSQL en render
    - Uso de gunicorn para levantar el servidor

--- config.py
+++ config.py
@@ -5,14 +5,10 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'clave_por_defecto')
-    
-    # Nueva clave de SerpApi
     SERPAPI_KEY = os.getenv('SERPAPI_KEY')
     
-    DB_USER = os.getenv('DB_USER', 'root')
-    DB_PASSWORD = os.getenv('DB_PASSWORD', '')
-    DB_HOST = os.getenv('DB_HOST', 'localhost')
-    DB_NAME = os.getenv('DB_NAME', 'agregador_perfumes')
+    # Intenta leer la URL de Render (Postgres), si no existe, usa la de XAMPP (MySQL)
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or \
+        f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
     
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
